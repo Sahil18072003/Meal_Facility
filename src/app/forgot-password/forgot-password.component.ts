a43345bd04e851eda2646f4a5fe4617fef260db3
Sahil Dharaviya <123103366+Sahil18072003@users.noreply.github.com>
2024-05-13 02:26:13 +0530

    first

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-forgot-password',
+  selector: 'app-login',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.css']
+  styleUrls: ['./forgot-password.component.css'],
 })
 export class ForgotPasswordComponent {
+  forgotPasswordForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.forgotPasswordForm = this.fb.group({
+      email: ['', Validators.required],
+    });
+  }
+
+  onSubmite() {
+    if (this.forgotPasswordForm.valid) {
+      console.log(this.forgotPasswordForm.value);
+      alert('Form Login successfully.');
+      //send data to database
+    } else {
+      console.log('Form is not valid');
+      //throw a error using toaster and with  required fileds
+      this.validdateAllFromFileds(this.forgotPasswordForm);
+      alert('Your form is invalid');
+    }
+  }
+
+  private validdateAllFromFileds(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach((field) => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsDirty({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validdateAllFromFileds(control);
+      }
+    });
+  }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+  hide = true;
+  type = 'password';
 
+  toggleVisibility(): void {
+    this.hide = !this.hide;
+    this.type == 'password' ? (this.type = 'text') : (this.type = 'password');
+  }
+
+  signupForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmite() {
+    if (this.signupForm.valid) {
+      console.log(this.signupForm.value);
+      alert('Form signup successfully.');
+      //send data to database
+    } else {
+      console.log('form is not valid');
+      //throw a error using toaster and with  required fileds
+      this.validdateAllFromFileds(this.signupForm);
+      alert('Your form is invalid');
+    }
+  }
+
+  private validdateAllFromFileds(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach((field) => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsDirty({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validdateAllFromFileds(control);
+      }
+    });
+  }
 }
